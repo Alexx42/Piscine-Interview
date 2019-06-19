ebbe19391d620b342ae7de69888020e68ec7973d
Alexandre Le goff <ale-goff@e1z2r5p31.42.us.org>
2019-06-18 17:40:27 -0700

    decompress

--- day04/ex16/decompress.c
+++ day04/ex16/decompress.c
@@ -7,14 +7,16 @@ int **pianoDecompress(struct s_bit *bit, int l) {
 	int		**res = malloc(sizeof(int *) * bit->n);
 	int i;
 	int j;
-	(void)bit;
 	for (i = 0; i < bit->n; i++)
 		res[i] = malloc(sizeof(int) * l);
 	for (i = 0; i < bit->n; i++) {
 		int count = 0;
 		for (j = 0; count < bit->arr[i]; j++) {
 			res[i][j] = 0;
-			count += pow(2, j);
+			if (count == 0)
+				count = 1;
+			else
+				count <<= 1;
 		}
 		res[i][j - 1] = 1;
 		for (; j < l; j++) {
