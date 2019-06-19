17742541e8331262d4b69e047110c8f9b6ccc511
Alexandre Le goff <ale-goff@e1z2r5p31.42.us.org>
2019-06-19 15:42:57 -0700

    all permutations

---
+++ day05/ex04/permutation.c
@@ -0,0 +1,29 @@
+#include "header.h"
+#include <string.h>
+#include <stdio.h>
+
+static void swap(char *s1, char *s2)
+{
+	char tmp = *s1;
+	*s1 = *s2;
+	*s2 = tmp;
+}
+
+static void printPermutationRec(char *str, int l, int r)
+{
+	if (l == r)
+	{
+		printf("%s\n", str);
+	}
+	for (int i = l; i <= r; i++)
+	{
+		swap(str + i, str +l);
+		printPermutationRec(str, l + 1, r);
+		swap(str + i, str +l);
+	}
+}
+
+void		printPermutations(char *str)
+{
+	printPermutationRec(str, 0, strlen(str) - 1);
+}
