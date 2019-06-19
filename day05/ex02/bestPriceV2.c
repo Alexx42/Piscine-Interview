14fc9a1cfe545d48c8c9da2a8a66f84be23d0378
Alexandre Le goff <ale-goff@e1z2r5p31.42.us.org>
2019-06-19 14:44:44 -0700

    best price v2

---
+++ day05/ex02/bestPriceV2.c
@@ -0,0 +1,29 @@
+#include "header.h"
+
+void optimizedBestPriceRec(double *prices, int size, int total, double price, double *res)
+{
+    for (int i = 0; i < 100; i++)
+    {
+        if (i == size)
+        {
+            if (res[i] < price)
+                res[i] = price;
+            break ;
+        }
+    }
+    if (total == size)
+        return ;
+    if (res[size] == price)
+    {
+        for (int i = 1; i <= total - size; i++)
+        {
+            optimizedBestPriceRec(prices, size + i, total, price + prices[i], res);
+        }
+    }
+}
+
+double optimizedBestPrice(int pizzaSize, double *prices) {
+    double res[100] = {0};
+    optimizedBestPriceRec(prices, 0, pizzaSize, 0, res);
+    return (res[pizzaSize]);
+}
