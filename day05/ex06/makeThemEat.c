325ec4b7b393b207dc40b5e0d17d7fb77adaa22c
Alexandre Le goff <ale-goff@e1z2r5p31.42.us.org>
2019-06-19 21:32:33 -0700

    make them eat

--- day05/ex06/makeThemEat.c
+++ day05/ex06/makeThemEat.c
@@ -1,10 +1,25 @@
 #include "header.h"
+#include <stdio.h>
+#include <stdlib.h>
+int isPossibleRec(struct s_people **people, int idx, int nbTable, int totalTime, int *arr)
+{
+	if (!people[idx])
+		return (1);
+	for (int i = 0; i < nbTable; i++)
+	{
+		if (arr[i] + people[idx]->time <= totalTime)
+		{
+			arr[i] += people[idx]->time;
+			if (isPossibleRec(people, idx + 1, nbTable, totalTime, arr))
+				return (1);
+			arr[i] -= people[idx]->time;
+		}
+	}
+	return (0);
+}
 
 int isPossible(struct s_people **people, int nbTable, int totalTime)
 {
-	for (int i = 0; people[i]; i++)
-	{
-		
-	}
-	return (0);
+	int		*arr = calloc(nbTable, sizeof(int));
+	return (isPossibleRec(people, 0, nbTable, totalTime, arr));
 }
